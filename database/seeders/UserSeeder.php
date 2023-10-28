f34f541951b54dba90214e40451a54445b583b72
Golveira <gabriel.golveira@gmail.com>
2023-10-28 14:59:57 -0300

    Refactor user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,14 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Bookmark;
 use App\Models\Dossier;
 use App\Models\User;
 use App\Models\Image;
-use App\Models\Like;
 use App\Models\Report;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -24,16 +21,6 @@ class UserSeeder extends Seeder
                 Report::factory()
                     ->count(3)
                     ->has(Image::factory()->count(1))
-            )
-            ->has(
-                Bookmark::factory()
-                    ->count(3)
-                    ->report()
-            )
-            ->has(
-                Bookmark::factory()
-                    ->count(3)
-                    ->dossier()
             )
             ->create();
 

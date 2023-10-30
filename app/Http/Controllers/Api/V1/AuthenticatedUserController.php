434ba4caa370381066f7fb3f468b281f97418542
Golveira <gabriel.golveira@gmail.com>
2023-10-30 12:17:42 -0300

    Add authenticated user controller

---
+++ app/Http/Controllers/Api/V1/AuthenticatedUserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+
+/**
+ * @group Users
+ *
+ * Endpoints for managing user.
+ */
+class AuthenticatedUserController extends Controller
+{
+    /**
+     * Get the authenticated user.
+     *
+     * Returns the authenticated user.
+     *
+     * @apiResource App\Http\Resources\UserResource
+     * @apiResourceModel App\Models\User
+     */
+    public function show(Request $request): UserResource
+    {
+        return new UserResource($request->user());
+    }
+
+    /**
+     * Update the authenticated user.
+     *
+     * Update the email and password of the authenticated user.
+     *
+     * @authenticated
+     * @apiResource App\Http\Resources\UserResource
+     * @apiResourceModel App\Models\User
+     */
+    public function update(UserRequest $request): UserResource
+    {
+        $request->user()->update($request->validated());
+
+        return new UserResource($request->user());
+    }
+}
--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
@@ -19,6 +18,7 @@ class UserController extends Controller
      * List users
      *
      * Get a list of users.
+     *
      * @apiResourceCollection App\Http\Resources\UserResource
      * @apiResourceModel App\Models\User paginate=15
      */
@@ -35,32 +35,16 @@ class UserController extends Controller
     /**
      * Get a user
      *
-     * Get a user by its ID.
-     * @urlParam id required The ID of the user. No-example
+     * Returns a single user specified by the ID.
+     *
+     * @urlParam id string required The ID of the user. No-example
      * @apiResource App\Http\Resources\UserResource
      * @apiResourceModel App\Models\User
      */
     public function show(User $user): UserResource
     {
-        return new UserResource($user->loadCount(['reports', 'dossiers']));
-    }
-
-    /**
-     * Update a user
-     *
-     * Update a user by its ID.
-     *
-     * @authenticated
-     * @urlParam id required The ID of the user. No-example
-     * @apiResource App\Http\Resources\UserResource
-     * @apiResourceModel App\Models\User
-     */
-    public function update(UserRequest $request, User $user): UserResource
-    {
-        $this->authorize('update', $user);
-
-        $user->update($request->validated());
-
-        return new UserResource($user);
+        return new UserResource(
+            $user->loadCount(['reports', 'dossiers'])
+        );
     }
 }

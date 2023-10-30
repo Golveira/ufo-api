b5f353dab9a6d427a3701bcd5959b4a941987e82
Golveira <gabriel.golveira@gmail.com>
2023-10-30 12:13:27 -0300

    Rename controllers

---
+++ app/Http/Controllers/Api/V1/AuthenticatedUserDossierController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DossierResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * @group Dossiers
+ *
+ * Endpoints for managing dossiers.
+ */
+class AuthenticatedUserDossierController extends Controller
+{
+    /**
+     * List dossiers for the authenticated user.
+     *
+     * Get a list of dossiers belonging to the authenticated user.
+     *
+     * @apiResourceCollection App\Http\Resources\DossierResource
+     * @apiResourceModel App\Models\Dossier paginate=15
+     */
+    public function index(Request $request): ResourceCollection
+    {
+        return DossierResource::collection(
+            $request->user()
+                ->dossiers()
+                ->withCount('reports')
+                ->latest()
+                ->paginate()
+        );
+    }
+}
---
+++ app/Http/Controllers/Api/V1/AuthenticatedUserReportController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ReportResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * @group Reports
+ *
+ * Endpoints for managing reports.
+ */
+class AuthenticatedUserReportController extends Controller
+{
+    /**
+     * List reports for the authenticated user.
+     *
+     * Get a list of reports belonging to the authenticated user.
+     *
+     * @apiResourceCollection App\Http\Resources\ReportResource
+     * @apiResourceModel App\Models\Report paginate=15
+     */
+    public function index(Request $request): ResourceCollection
+    {
+        return ReportResource::collection(
+            $request->user()
+                ->reports()
+                ->latest()
+                ->paginate()
+        );
+    }
+}
